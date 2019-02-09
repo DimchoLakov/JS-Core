604b8dc831b70aa4bf86a350c889fa9887d56d5f
whywherewhat <mdsektor@gmail.com>
2019-02-09 10:00:10 +0000

    Added solutions to problem Locked Profile

--- 01. JS Fundamentals/02. DOM Exercises/04_Locked-Profile/solution.js
+++ 01. JS Fundamentals/02. DOM Exercises/04_Locked-Profile/solution.js
@@ -1,3 +1,27 @@
 function solve() {
-   //TODO
-} 
+
+    let buttons = document.getElementsByTagName('button');
+
+    for (let button of buttons) {
+        button.addEventListener('click', function (e) {
+
+            let currentButton = e.target;
+            let parentNode = currentButton.parentNode;
+            let unlockedInput = parentNode.children[4];
+
+            if (unlockedInput.checked) {
+                let hiddenDiv = parentNode.children[9];
+                let hiddenDivDisplay = hiddenDiv.style.display;
+
+                if (hiddenDivDisplay === 'inline') {
+                    hiddenDiv.style.display = 'none';
+                    currentButton.textContent = 'Show more';
+
+                } else if (hiddenDivDisplay === '' || hiddenDivDisplay === 'none') {
+                    hiddenDiv.style.display = 'inline';
+                    currentButton.textContent = 'Hide it';
+                }
+            }
+        })
+    }
+}
