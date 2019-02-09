311e0fd31f620da67e0bf552f986ad5de3ff8437
whywherewhat <mdsektor@gmail.com>
2019-02-09 20:10:50 +0000

    Added solution for Table Search Engine

--- 01. JS Fundamentals/02. DOM Exercises/07_Table-Search-Engine/solution.js
+++ 01. JS Fundamentals/02. DOM Exercises/07_Table-Search-Engine/solution.js
@@ -1,3 +1,28 @@
 function solve() {
-    //TODO
+    let searchButton = document.getElementById('searchBtn');
+
+    searchButton.addEventListener('click', function () {
+        let searchField = document.getElementById('searchField');
+        let searchStr = searchField.value;
+        if (searchStr === '') {
+            return;
+        }
+
+        let regex = new RegExp(searchStr, 'i');
+
+        let cells = Array.from(document.querySelectorAll('tbody tr td'));
+
+        for (let cell of cells) {
+            cell.parentNode.classList.remove('select');
+        }
+
+        for (let cell of cells) {
+            let match = regex.exec(cell.textContent);
+            if (match !== null) {
+                cell.parentNode.classList.add('select');
+            }
+        }
+
+        searchField.value = '';
+    })
 }
