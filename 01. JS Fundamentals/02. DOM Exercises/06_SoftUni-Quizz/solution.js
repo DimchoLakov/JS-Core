6242208fea5c84b46ca4a8439d00cf4222803556
whywherewhat <mdsektor@gmail.com>
2019-02-09 19:26:30 +0000

    Added solution for Softuni Quizz

--- 01. JS Fundamentals/02. DOM Exercises/06_SoftUni-Quizz/solution.js
+++ 01. JS Fundamentals/02. DOM Exercises/06_SoftUni-Quizz/solution.js
@@ -1,3 +1,48 @@
 function solve() {
-	//TODO
+    let rightAnswers = ['2013', 'Pesho', 'Nakov'];
+    let correctAnswersCount = 0;
+
+    let buttons = Array.from(document.getElementsByTagName('button'));
+
+    for (let button of buttons) {
+        button.addEventListener(('click'), function (e) {
+
+            let target = e.target;
+            let parentNode = target.parentNode;
+            let arr = Array.from(parentNode.children);
+
+            let radioButtons = arr.filter(function (x) {
+                return x.tagName.toLocaleLowerCase() === 'input' && x.checked;
+            });
+
+            let selectedValue = '';
+
+            if (radioButtons.length === 0) {
+                return;
+            }
+
+            selectedValue = radioButtons[0].value;
+
+            for (let i = 0; i < rightAnswers.length; i++) {
+                if (rightAnswers[i] === selectedValue) {
+                    correctAnswersCount++;
+                }
+            }
+
+            let buttonIndex = buttons.indexOf(button);
+            if (buttonIndex < 2) {
+                let nextSection = Array.from(document.getElementsByTagName('section'))[buttonIndex + 1];
+                nextSection.classList.remove('hidden');
+            } else {
+
+                let resultDiv = document.getElementById('result');
+
+                if (correctAnswersCount === 3) {
+                    resultDiv.textContent = 'You are recognized as top SoftUni fan!';
+                } else {
+                    resultDiv.textContent = `You have ${correctAnswersCount} right answers`;
+                }
+            }
+        })
+    }
 }
