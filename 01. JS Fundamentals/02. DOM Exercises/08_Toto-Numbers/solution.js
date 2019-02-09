96bf704d48cdcf6995121bf450cd7cb216030ba6
whywherewhat <mdsektor@gmail.com>
2019-02-09 21:46:38 +0000

    Added solution for Toto Numbers

--- 01. JS Fundamentals/02. DOM Exercises/08_Toto-Numbers/solution.js
+++ 01. JS Fundamentals/02. DOM Exercises/08_Toto-Numbers/solution.js
@@ -1,3 +1,42 @@
 function solve() {
-	//TODO
+	let button = document.getElementsByTagName('button')[0];
+
+	button.addEventListener('click', function () {
+	    let inputField = document.querySelector('#myNumbers input');
+        let inputNumbers = inputField.value;
+        let numbers = inputNumbers.split(' ').map(function (x) {
+            return Number(x);
+        });
+
+        // check if numbers count is 6
+        if (numbers.length !== 6){
+            return;
+        }
+
+        // check if numbers are in range 1-49
+        for (let number of numbers) {
+            if (number < 1 || number > 49 || isNaN(number)){
+                return;
+            }
+        }
+
+        let allNumbersDiv = document.getElementById('allNumbers');
+
+        for (let i = 1; i <= 49; i++) {
+            let div = document.createElement('div');
+            div.textContent = i.toString();
+            div.classList.add('numbers');
+
+            for (let j = 0; j < numbers.length; j++) {
+                let currentNumber = numbers[j];
+                if (currentNumber === i){
+                    div.style.backgroundColor = 'orange';
+                }
+            }
+
+            allNumbersDiv.appendChild(div);
+        }
+        button.disabled = true;
+        inputField.disabled = true;
+    })
 }
