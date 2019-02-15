4591448fdea6b5210054d474cffad0fd2f242329
whywherewhat <mdsektor@gmail.com>
2019-02-15 21:15:05 +0000

    Added solution for Leap Year

--- 01. JS Fundamentals/04. Functions and Logic Flow - Exercises/01_Leap-Year/solution.js
+++ 01. JS Fundamentals/04. Functions and Logic Flow - Exercises/01_Leap-Year/solution.js
@@ -1,3 +1,26 @@
 function leapYear() {
-    // TODO:
+
+    let checkButton = document.querySelectorAll('button')[0];
+    checkButton.addEventListener('click', () => {
+
+        let input = document.querySelector('#exercise input[type=number]');
+        let year = Number(input.value);
+        let h2 = document.querySelector('#year h2');
+        let divResult = document.querySelector('#year div');
+
+        if (isLeapYear(year)) {
+            h2.textContent = 'Leap Year';
+        } else {
+            h2.textContent = 'Not Leap Year';
+        }
+
+        divResult.textContent = year.toString();
+
+        input.value = '';
+    })
+
+
+    function isLeapYear(year) {
+        return ((year % 4 === 0) && (year % 100 !== 0)) || (year % 400 === 0);
+    }
 }
