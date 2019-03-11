9c7ae2cd2125d729b7271a76bf0cc2b3200ab0ed
DimchoLakov <mdsektor@gmail.com>
2019-03-11 01:05:03 +0000

    Added solutions for 01.Shopping-Cart, 03.Forum

--- 01. JS Fundamentals/06. Objects and JSON - Exercises/01.Shopping-Cart/solution.js
+++ 01. JS Fundamentals/06. Objects and JSON - Exercises/01.Shopping-Cart/solution.js
@@ -1,3 +1,39 @@
 function solve() {
-    // TODO
+    let addButtons = document.querySelectorAll(".product button");
+
+    let boughtProducts = [];
+    let total = 0;
+
+    for (let button of addButtons) {
+        button.addEventListener("click", function (e) {
+            let target = e.target;
+            let parentNode = target.parentNode;
+            let children = parentNode.children;
+
+            let productName = children[1].textContent;
+            let priceAsStr = children[2].textContent.split(/[:\s]+/g).filter(x => x !== "")[1];
+            let price = Number(priceAsStr);
+
+            total += price;
+
+            if (!boughtProducts.includes(productName)) {
+                boughtProducts.push(productName);
+            }
+
+            let message = `Added ${productName} for ${price} to the cart.\n`;
+
+            let textarea = document.querySelector("textarea");
+            textarea.textContent += message;
+        })
+    }
+
+    let buyButton = document.querySelectorAll("button")[3];
+
+    buyButton.addEventListener("click", function (e) {
+
+        let message = `You bought ${boughtProducts.join(", ")} for ${total.toFixed(2)}.\n`;
+
+        let textarea = document.querySelector("textarea");
+        textarea.textContent += message;
+    });
 }
--- 01. JS Fundamentals/06. Objects and JSON - Exercises/03.Forum/solution.js
+++ 01. JS Fundamentals/06. Objects and JSON - Exercises/03.Forum/solution.js
@@ -1,3 +1,60 @@
 function solve() {
- // TODO
+
+    let buttons = document.querySelectorAll("button");
+
+    let submitButton = buttons[0];
+    submitButton.addEventListener("click", function (event) {
+        // prevent submit button from sending the form(default action)
+        event.preventDefault();
+
+        let userInputs = document.querySelectorAll(".user-info input");
+        let username = userInputs[0].value;
+        let email = userInputs[2].value;
+
+        let topicsArr = Array.from(document.querySelectorAll(".topics input"));
+        let topics = topicsArr
+            .filter(x => x.checked)
+            .map(x => x.value)
+            .join(" ");
+
+        let tdUsername = document.createElement('td');
+        tdUsername.textContent = username;
+        let tdEmail = document.createElement('td');
+        tdEmail.textContent = email;
+        let tdTopics = document.createElement('td');
+        tdTopics.textContent = topics;
+
+        let tr = document.createElement('tr');
+        tr.appendChild(tdUsername);
+        tr.appendChild(tdEmail);
+        tr.appendChild(tdTopics);
+
+        let tbody = document.querySelector("table tbody");
+        tbody.appendChild(tr);
+
+        // set input field values to ""
+        userInputs.forEach(i => i.value = "");
+        topicsArr.forEach(x => x.checked = false);
+    });
+
+    let searchButton = buttons[1];
+    searchButton.addEventListener("click", function (event) {
+
+        let searchStr = document.querySelectorAll("input")[7].value;
+        let rows = Array.from(document.querySelector("table tbody").children);
+
+        rows.forEach(el => el.style.visibility = "hidden");
+
+        rows.filter(el => Array.from(el.children).some(x => x.textContent.includes(searchStr)))
+            .forEach(el => el.style.visibility = "visible");
+
+        /*for (let row of rows) {
+
+            let rowChildren = Array.from(row.children);
+            if (rowChildren.every(x => !x.textContent.includes(searchStr))) {
+                row.style.visibility = "hidden";
+            }
+
+        }*/
+    });
 }
