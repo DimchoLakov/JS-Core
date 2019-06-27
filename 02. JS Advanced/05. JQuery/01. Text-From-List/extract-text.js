6de572df4e12e5cb8e6b0ac7f03ad2865f3e0509
DimchoLakov <mdsektor@gmail.com>
2019-06-27 19:15:26 +0100

    Add resources

--- 02. JS Advanced/05. JQuery/01. Text-From-List/extract-text.js
+++ 02. JS Advanced/05. JQuery/01. Text-From-List/extract-text.js
@@ -1,3 +1,12 @@
 function extractText() {
-   // TODO:
+   let listItems = $('#items').children().toArray();
+   let values = listItems.map(x => x.textContent);
+   let result = values.join(', ');
+   console.log(result);
+   $('#result').append(result);
+   BootstrapDialog.alert('Something!');
+   BootstrapDialog.alert('I want banana!');
+   BootstrapDialog.show({
+      message: 'Hi Apple!'
+   });
 }
--- 02. JS Advanced/05. JQuery/02. Search-In-List/search.js
+++ 02. JS Advanced/05. JQuery/02. Search-In-List/search.js
@@ -1,3 +1,9 @@
 function search() {
-   // TODO:
+    let searchStr = $('#searchText').val();
+    let towns = $('#towns')
+        .children();
+    for (let town of towns) {
+        console.log(town.textContent);
+    }
+    console.log(towns);
 }
