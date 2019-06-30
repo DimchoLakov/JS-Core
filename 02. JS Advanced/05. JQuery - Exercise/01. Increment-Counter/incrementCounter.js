7fd25dc2b898494c1be8eab0e2e5a499294d6f62
DimchoLakov <mdsektor@gmail.com>
2019-06-30 02:52:38 +0100

    Add solutions

--- 02. JS Advanced/05. JQuery - Exercise/01. Increment-Counter/incrementCounter.js
+++ 02. JS Advanced/05. JQuery - Exercise/01. Increment-Counter/incrementCounter.js
@@ -1,3 +1,35 @@
 function increment(selector) {
-    // TODO: 
+    let $textarea = $('<textarea>');
+    $textarea.addClass('counter');
+    $textarea.val(0);
+    $textarea.attr('disabled', 'true');
+
+    let $incrementBtn = $('<button>');
+    $incrementBtn.addClass('btn');
+    $incrementBtn.attr('id', 'incrementBtn');
+    $incrementBtn.text('Increment');
+
+    let $addBtn = $('<button>');
+    $addBtn.addClass('btn');
+    $addBtn.attr('id', 'addBtn');
+    $addBtn.text('Add');
+
+    let $ul = $('<ul>');
+    $ul.addClass('results');
+
+    let result = $(`${selector}`);
+    result.append($textarea);
+    result.append($incrementBtn);
+    result.append($addBtn)
+    result.append($ul);
+
+    $incrementBtn.on('click', function () {
+        let currentVal = $textarea.val();
+        $textarea.val(Number(currentVal) + 1);
+    });
+
+    $addBtn.on('click', function () {
+       let $li = $('<li>').text($textarea.val().toString());
+       $ul.append($li);
+    });
 }
--- 02. JS Advanced/05. JQuery - Exercise/02. Timer/timer.js
+++ 02. JS Advanced/05. JQuery - Exercise/02. Timer/timer.js
@@ -1,3 +1,67 @@
 function timer() {
-   // TODO:
+    let $timer;
+    let isTimerRunning = false;
+
+    let totalSeconds = 0;
+
+    let $startTimerBtn = $('#start-timer');
+    let $stopTimerBtn = $('#stop-timer');
+    let $resetTimerBtn = $('#reset-timer');
+
+    $startTimerBtn.on('click', start);
+
+    $stopTimerBtn.on('click', stop);
+
+    $resetTimerBtn.on('click', reset);
+
+    function step() {
+        totalSeconds += 1;
+        let seconds = totalSeconds % 60;
+        let minutes = Math.floor(totalSeconds / 60) % 60;
+        let hours = Math.floor(totalSeconds / 60 / 60);
+
+        let secondToString = seconds.toString();
+        let minutesToString = minutes.toString();
+        let hoursToString = hours.toString();
+
+        if (secondToString.length === 1) {
+            secondToString = `0${secondToString}`;
+        }
+        if (minutesToString.length === 1) {
+            minutesToString = `0${minutesToString}`;
+        }
+        if (hoursToString.length === 1) {
+            hoursToString = `0${hoursToString}`;
+        }
+
+        $('#seconds').text(secondToString);
+        $('#minutes').text(minutesToString);
+        $('#hours').text(hoursToString);
+    }
+
+    function start() {
+        if (isTimerRunning === false) {
+            $startTimerBtn.text('Pause');
+            $timer = setInterval(step, 1000);
+            isTimerRunning = true;
+        } else {
+            clearInterval($timer);
+            $startTimerBtn.text('Start');
+            isTimerRunning = false;
+        }
+    }
+
+    function reset() {
+        totalSeconds = 0;
+        $('#seconds').text('00');
+        $('#minutes').text('00');
+        $('#hours').text('00');
+    }
+
+    function stop() {
+        reset();
+        clearInterval($timer);
+        $startTimerBtn.text('Start');
+        isTimerRunning = false;
+    }
 }
